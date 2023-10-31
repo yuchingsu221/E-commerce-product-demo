7041206611e679ceeae3adb7a60b9198f094d277
DESKTOP-EOFQD5R\yuching <williamsu@softmobile.com.tw>
2023-10-31 23:34:18 +0800

    建置起始 product class 與 README

---
+++ API/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. 看是否為開發 mode.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection(); 暫時不需要 https
+
+app.UseAuthorization();
+
+app.MapControllers(); // request 進來的資料要傳映射到哪個 controller
+
+app.Run();
